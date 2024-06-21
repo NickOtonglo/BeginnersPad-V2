979401c4171d98d40a23ec2687b5b8acffe86487
Nick Otonglo <nickotonglo@gmail.com>
2024-06-21 17:04:04 +0100

    added 'status' fields with a check for the subscription expiry time vs the present time; adjusted the parameters for the 'time_left' value of the resource array

--- app/Http/Resources/PremiumPlanSubscription/SubscriptionResource.php
+++ app/Http/Resources/PremiumPlanSubscription/SubscriptionResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\PremiumPlanSubscription;
 use App\Models\PremiumPlan;
 use App\Models\User;
 use Carbon\Carbon;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,8 +18,22 @@ class SubscriptionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $status = '';
         $user = User::find($this->user_id);
         $plan = PremiumPlan::find($this->premium_plan_id);
+
+        // https://stackoverflow.com/questions/19031235/php-check-if-date-is-past-to-a-certain-date-given-in-a-certain-format
+        $expiry = new DateTime($this->expires_at);
+        $now = new DateTime();
+
+        if ($expiry > $now) {
+            // expiry is in the future, valid subscripton is already in effect
+            $status = 'active';
+        } else {
+            $status = 'inactive';
+        }
+
+        // https://laraveldaily.com/post/carbon-diffforhumans-parameters-and-extra-options
 
         if ($plan->slug == 'waiting-list') {
             return [
@@ -27,10 +42,16 @@ class SubscriptionResource extends JsonResource
                 'activated_at' => Carbon::parse($this->activated_at)->format('jS F Y, H:m:s'),
                 'time_ago' => Carbon::parse($this->activated_at)->diffForHumans(),
                 'expires_at' => Carbon::parse($this->expires_at)->format('jS F Y, H:m:s'),
-                'time_left' => Carbon::parse($this->expires_at)->diffForHumans(),
+                'time_left' => Carbon::parse($this->expires_at)->diffForHumans(
+                    now(),
+                    Carbon::DIFF_RELATIVE_TO_NOW,
+                    false,
+                    2
+                ),
                 'subscriber' => $user->username,
                 'plan' => $plan->name,
                 'waiting_lists' => WaitingListResource::collection($this->waitingLists),
+                'status' => $status
             ];
         }
 
@@ -43,6 +64,7 @@ class SubscriptionResource extends JsonResource
             'time_left' => Carbon::parse($this->expires_at)->diffForHumans(),
             'subscriber' => $user->username,
             'plan' => $plan->name,
+            'status' => $status
         ];
     }
 }
