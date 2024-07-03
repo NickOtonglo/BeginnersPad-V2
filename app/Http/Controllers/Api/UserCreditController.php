234415e886d57d7b6dc8a9db6bf7afe2f0b03310
Nick Otonglo <nickotonglo@gmail.com>
2024-07-03 15:44:20 +0100

    updated relationship function ('user()->userCredit' to 'user()->credit'); updated function parameters and structure of store(), show() and update() functions; added function prePost() to determine if incoming api request will be treated as a create or update function execution for user's credit account; added function to check if user has credit account; added function to create logs for UserCredit outside middleware

--- app/Http/Controllers/Api/UserCreditController.php
+++ app/Http/Controllers/Api/UserCreditController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\User\UserCreditResource;
 use App\Models\User;
+use App\Models\UserActivityLog;
 use App\Models\UserCredit;
+use App\Models\UserCreditLog;
 use Illuminate\Http\Request;
-use Ramsey\Uuid\Type\Decimal;
 
 class UserCreditController extends Controller
 {
@@ -16,7 +17,7 @@ class UserCreditController extends Controller
      */
     public function index()
     {
-        $credit = auth()->user()->userCredit;
+        $credit = auth()->user()->credit;
         return $credit;
     }
 
@@ -31,30 +32,34 @@ class UserCreditController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Decimal $amount, User $user)
+    public function store(float $amount, User $user, string $source = null)
     {
         $data = new UserCredit();
         $data->user_id = $user->id;
         $data->credit = $amount;
         $data->save();
-        $comment = '';
 
-        $response = [
-            'user_credit' => $data,
-            'model' => 'UserCredit',
-            'key' => $data->id,
-            'message' => "Credit account for '".$user->name."' created successfully.",
-            'comment' => $comment,
-        ];
-        return response($response, 201);
+        if (!$source) {
+            $comment = '';
+
+            $response = [
+                'user_credit' => $data,
+                'model' => 'UserCredit',
+                'key' => $data->id,
+                'message' => "Credit account for '".$user->name."' created successfully.",
+                'comment' => $comment,
+            ];
+            return response($response, 201);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show()
     {
-        return new UserCreditResource($user->userCredit);
+        $user = auth()->user();
+        return new UserCreditResource($user->credit);
     }
 
     /**
@@ -68,11 +73,11 @@ class UserCreditController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UserCredit $data, Decimal $amount = 0, bool $autoPay = true, User $user)
+    public function update(UserCredit $data, float $amount = 0, bool $autoPay = false, User $user, string $source = null, bool $log = false)
     {
         $credit = UserCredit::find($data->id);
         
-        $data->amount = $data->amount + $amount;
+        $data->credit = $data->credit + $amount;
         $data->auto_pay = $autoPay;
 
         $comment = '';
@@ -85,14 +90,20 @@ class UserCreditController extends Controller
 
         $data->save();
 
-        $response = [
-            'user_credit' => $data,
-            'model' => 'UserCredit',
-            'key' => $data->id,
-            'message' => "Credit account for '".$user->name."' updated successfully.",
-            'comment' => $comment,
-        ];
-        return response($response, 201);
+        if ($log) {
+            $this->createLog($data, $comment, 'UserCredit', $data->id);
+        }
+
+        if (!$source){
+            $response = [
+                'user_credit' => $data,
+                'model' => 'UserCredit',
+                'key' => $data->id,
+                'message' => "Credit account for '".$user->name."' updated successfully.",
+                'comment' => $comment,
+            ];
+            return response($response, 201);
+        }
     }
 
     /**
@@ -101,5 +112,76 @@ class UserCreditController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function prePost(Request $request, string $username = '') {
+        $user = '';
+        if (!$username) {
+            $user = auth()->user();
+        } else {
+            $user = User::where('username', $username)->first();
+        }
+        $credit = '';
+        $amount = 0;
+        $autoPay = '';
+
+        if ($user->credit) {
+            $credit = $user->credit;
+            if ($request->auto_pay) {
+                $autoPay = $request->auto_pay;
+            }
+            if ($request->amount) {
+                $amount = $request->amount;
+            }
+            $this->update($credit, $amount, $autoPay, $user);
+        } else {
+            $credit = new UserCredit();
+            $autoPay = $request->auto_pay;
+            $amount = $request->amount;
+            $this->store($amount, $user);
+        }
+    }
+
+    public function doesUserHaveCreditAccount() {
+        $user = auth()->user();
+
+        if ($user->credit) {
+            return true;
+        }
+        return false;
+    }
+
+    public function createLog(UserCredit $credit, $comment, $model, $key) {
+        $data = new UserCreditLog();
+
+        $data->user_id = $credit->user_id;
+        $data->credit = $credit->credit;
+        $data->auto_pay = $credit->auto_pay;
+
+        if ($comment) {
+            $data->comment = $comment;
+        } else {
+            $data->comment = $model.' #'.$key.': '.request()->method();
+        }
+
+        $log = [];
+        if (!auth()->user() && request()->email) {
+            $log['email'] = request()->email;
+            $log['is_registered'] = false;
+        } else {
+            $log['email'] = auth()->user()->email;
+            $log['is_registered'] = true;
+        }
+        $log['ip_address'] = request()->ip();
+        $log['url'] = request()->fullUrl();
+        $log['method'] = request()->method();
+        $log['client'] = request()->header('user_agent');
+        $log['model'] = $model;
+        $log['model_id'] = $key;
+        $data->parent_id = UserActivityLog::create($log)->id;
+
+        $data->save();
+
+        // app(NotificationsController::class)->sendUserCreditNotification($credit, $comment);
     }
 }
