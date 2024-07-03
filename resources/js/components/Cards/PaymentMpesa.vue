1771556e88bf023da7f1ddfb9dcd4b6a76999cd3
Nick Otonglo <nickotonglo@gmail.com>
2024-07-03 16:00:25 +0100

    added placeholder card for payment gateway view

---
+++ resources/js/components/Cards/PaymentMpesa.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card">
+        <h4 class="title-main txt-single-line">To pay via M-Pesa, follow the steps below:</h4>
+        <p class="text-main txt-six-line">1.</p> 
+        <p class="text-main txt-six-line">2.</p> 
+        <p class="text-main txt-six-line">3.</p> 
+    </div>
+</template>
