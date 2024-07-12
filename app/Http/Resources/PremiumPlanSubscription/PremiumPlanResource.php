d17a22498a50a4954f69475363451ffde1f1efdc
Nick Otonglo <nickotonglo@gmail.com>
2024-07-12 20:23:51 +0100

    added key 'sub_limit' which contains as its value the waiting list subscription limit for premium subscribers

--- app/Http/Resources/PremiumPlanSubscription/PremiumPlanResource.php
+++ app/Http/Resources/PremiumPlanSubscription/PremiumPlanResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\PremiumPlanSubscription;
 
+use App\Http\Controllers\Api\SystemController;
+use App\Models\System;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,12 +16,19 @@ class PremiumPlanResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $limit = '';
+
+        if ($this->slug == 'waiting-list') {
+            $limit = app(SystemController::class)->main(System::where('key', 'waiting_list_sub_limit')->first());
+        }
+
         return [
             'name' => $this->name,
             'description' => $this->description,
             'status' => $this->status,
             'minimum_days' => $this->minimum_days,
             'price' => $this->price,
+            'sub_limit' => intval($limit->value),
         ];
     }
 }
