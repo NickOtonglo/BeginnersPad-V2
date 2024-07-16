d7a881ed599f4c3b3ee4ea9392589259fbabd928
Nick Otonglo <nickotonglo@gmail.com>
2024-07-16 16:32:47 +0100

    refactored the navigation drawer composable file

--- resources/js/composables/nav-drawer.js
+++ resources/js/composables/nav-drawer.js
@@ -1,40 +1,27 @@
 import checkAuth from '../composables/checkAuth';
 
-export default function operateNavDrawer() {
+export default function navDrawerMaster() {
     const userAuth = checkAuth()
-
     let body = document.body;
 
-    if (userAuth.isAuthenticated.value) {
-        if(document.querySelector('#navDrawerOpen')) {
-            let navDrawerOpen = document.querySelector('#navDrawerOpen');
-        }
-        if(document.querySelector('#navDrawerClose')) {
-            let navDrawerClose = document.querySelector('#navDrawerClose');
-        }
-        if(document.querySelector('#navDrawer')) {
-            let navDrawer = document.querySelector('#navDrawer');
-        }
-        // if (document.querySelector('#pageFiller')){
-        //     pageFiller = document.querySelector('#pageFiller');
-        // }
-
-        navDrawerOpen.onclick = function(){
+    const openDrawer = () => {
+        if (userAuth.isAuthenticated.value) {
             navDrawer.classList.remove('closed');
             body.classList.add('unfocused');
             pageFiller.classList.add('filler');
-        };
-        
-        navDrawerClose.onclick = function(){
-            navDrawer.classList.add('closed');
-            body.classList.remove('unfocused');
-            pageFiller.classList.remove('filler');
         }
+    }
 
-        pageFiller.onclick = function(){
+    const closeDrawer = () => {
+        if (userAuth.isAuthenticated.value) {
             navDrawer.classList.add('closed');
             body.classList.remove('unfocused');
             pageFiller.classList.remove('filler');
         }
+    }
+
+    return {
+        openDrawer, 
+        closeDrawer
     }
 }
