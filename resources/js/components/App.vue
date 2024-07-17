4ec9c2f4101f8b29093bed5fa5f805d15e6c657c
Nick Otonglo <nickotonglo@gmail.com>
2024-07-17 17:22:45 +0100

    added closeDrawer() function as prop to all user navigation drawers

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -48,9 +48,9 @@
                     <li><a href="#" @click="userLogin.logout"><i class="fas fa-sign-out-alt"></i> Sign out</a></li>
                 </ul>
             </div>
-            <NavDrawerAdmin v-if="user.role == 'Admin' || user.role == 'Super Admin' || user.role == 'System Admin'" />
-            <NavDrawerLister v-if="user.role == 'Lister'" />
-            <NavDrawerBeginner v-if="user.role == 'Beginner'" />
+            <NavDrawerAdmin :closeDrawer="closeDrawer" v-if="user.role == 'Admin' || user.role == 'Super Admin' || user.role == 'System Admin'" />
+            <NavDrawerLister :closeDrawer="closeDrawer" v-if="user.role == 'Lister'" />
+            <NavDrawerBeginner :closeDrawer="closeDrawer" v-if="user.role == 'Beginner'" />
         </div>
     </template>
     <!-- beginner -->
