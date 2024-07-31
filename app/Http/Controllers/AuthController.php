fa280c82d142ea9c26589d46c7381930d099dacd
Nick Otonglo <nickotonglo@gmail.com>
2024-07-31 16:21:08 +0100

    added 'remember' parameter to attempt() function to remember users who check the 'remember me' checkbox during login; added forgotPassword() and resetPassword() functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Api\UserCreditController;
 use App\Http\Requests\SaveUserRequest;
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -41,7 +44,7 @@ class AuthController extends Controller
         ]);
 
         $credentials = request(['email', 'password']);
-        if (!auth()->attempt($credentials)) {
+        if (!auth()->attempt($credentials, $request->remember)) {
             return response()->json([
                 'message' => 'The given data was invalid. Please check your credentials and try again.',
                 'errors' => [
@@ -81,5 +84,44 @@ class AuthController extends Controller
         }
 
         return redirect('/');
+    }
+
+    public function forgotPassword(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+     
+        return $status === Password::RESET_LINK_SENT
+                    ? back()->with(['status' => __($status)])
+                    : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function resetPassword(Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => bcrypt($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+                ? with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
     }
 }
