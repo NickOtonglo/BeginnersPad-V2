62e9f811ec819fff2f4f45ca108472bf7d11df2c
Nick Otonglo <nickotonglo@gmail.com>
2024-07-31 16:33:16 +0100

    added submitEmail() and submitNewPassword() properties as functions

--- resources/js/composables/login.js
+++ resources/js/composables/login.js
@@ -41,6 +41,61 @@ export default function loginUser() {
         })
     }
 
+    const submitEmail = async() => {
+        if (isLoading.value) return
+        isLoading.value = true
+
+        axios.post(`/forgot-password`, credentials)
+            .then(response => {
+                swal({
+                    icon: 'success',
+                    title: 'Password reset link sent.',
+                    text: 'A password reset link has been sent to the email address you provided. Kindly follow the instructions given within to reset your password.',
+                    didClose: () => {
+                        router.go(-1)
+                    }
+                })
+            })
+            .catch(error => {
+                if (error.response?.data) {
+                    validationErrors.value = error.response.data.errors
+                }
+                console.log(error)
+            })
+            .finally(isLoading.value = false)
+    }
+
+    const submitNewPassword = async(data) => {
+        if (isLoading.value) return
+        isLoading.value = true
+
+        let serialisedPost = new FormData()
+        serialisedPost.append('password', data[0].password)
+        serialisedPost.append('password_confirmation', data[0].password_confirmation)
+        serialisedPost.append('email', data[1])
+        serialisedPost.append('token', data[2])
+
+        axios.post(`/reset-password`, serialisedPost)
+            .then(response => {
+                swal({
+                    icon: 'success',
+                    title: 'Your password has been reset.',
+                    text: 'You may now sign in with your new password.',
+                    didClose: () => {
+                        // router.go(-1)
+                        router.push({ name: 'auth.login' })
+                    }
+                })
+            })
+            .catch(error => {
+                if (error.response?.data) {
+                    validationErrors.value = error.response.data.errors
+                }
+                console.log(error)
+            })
+            .finally(isLoading.value = false)
+    }
+
     const authenticate = (response) => {
         localStorage.setItem('authenticated', JSON.stringify(true))
         localStorage.setItem('authToken', response.data.token)
@@ -64,6 +119,7 @@ export default function loginUser() {
                 router.go()
             })
             .catch(error => console.log(error.response))
+            .finally(isLoading.value = false)
     }
 
     return { 
@@ -71,6 +127,8 @@ export default function loginUser() {
         validationErrors, 
         isLoading, 
         submitLogin, 
+        submitEmail, 
+        submitNewPassword, 
         logout, 
         route,
     }
