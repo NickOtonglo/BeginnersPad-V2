4a19f6a2637136d5868240c12663107a581f0ba0
Nick Otonglo <nickotonglo@gmail.com>
2024-07-31 16:36:31 +0100

    added routes for 'forgot password' and 'reset password' features

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('sign-in', '\App\Http\Controllers\AuthController@login')->name('login');
 Route::post('sign-out', '\App\Http\Controllers\AuthController@logout')->middleware(['auth:sanctum'])->name('logout');
 Route::post('sign-up', '\App\Http\Controllers\AuthController@register');
+Route::post('forgot-password', '\App\Http\Controllers\AuthController@forgotPassword');
+Route::get('/reset-password/{token}', function (string $token) {
+    return view('app', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+Route::post('reset-password', '\App\Http\Controllers\AuthController@resetPassword');
 
 // Route::view('/', 'app');
 
