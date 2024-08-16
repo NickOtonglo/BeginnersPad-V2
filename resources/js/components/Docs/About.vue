4f3155eac047e4d17f9392891f0f2d1f4383ea51
Nick Otonglo <nickotonglo@gmail.com>
2024-08-16 19:46:31 +0100

    added new components for static components to be accessed mainly from the home page

---
+++ resources/js/components/Docs/About.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="showcase-small">
+        <div class="showcase-overlay">
+            <div class="container">
+                <div>
+                    <h3>Finding your next home doesn’t need to be hard, or expensive! If you are just starting out in
+                        life, or if you’re in need of a cost-friendly home, you can count on us!</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-read">
+        <div class="container">
+            <div class="list">
+                <h3 class="section-title">About Beginners Pad</h3>
+                <div class="item">
+                    <h4>What is Beginners Pad?</h4>
+                    <p>
+                        Beginners Pad is a property listing application that is specifically tailored to meeting the needs
+                        of people looking for affordable homes without any hassle. We don’t allow just any properties to be
+                        listed on our platform. We ensure that whatever properties are listed are thoroughly inspected
+                        before they are approved and published, to ensure that their representation on the platform is an
+                        accurate reflection of the actual properties in their physical form. We strongly believe that as a
+                        customer, you have the right to know exactly what you’re moving into, and we hope that you can trust
+                        us to give you the right environment for that.
+                    </p>
+                </div>
+                <div class="item">
+                    <h4>Our partners</h4>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div class="item" id="who-we-are">
+                    <h4>Who we are</h4>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
