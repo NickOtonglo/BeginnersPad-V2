168066a5d204ff74ac530aad3d260983f52fcf2e
Nick Otonglo <nickotonglo@gmail.com>
2024-08-16 22:38:46 +0100

    added 'Cookies' modal component

---
+++ resources/js/components/Modals/Cookies.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="modal-floating cookies">
+        <div class="content">
+            <p>
+                By using this application, you agree to our <router-link :to="{ name: 'app.terms' }"><b><u>Terms of
+                    Service</u></b></router-link> and our use of cookies to make your experience better.
+            </p>
+        </div>
+        <div class="options">
+            <button @click="setConsent(), $emit('consented', consent)"
+                class="btn-dark-outline btn-link">Understood</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const consent = ref(false)
+
+function setConsent() {
+    consent.value = localStorage.setItem('cookiesConsented', JSON.stringify(true))
+}
+</script>
