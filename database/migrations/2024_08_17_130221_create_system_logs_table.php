b40059ff4b998fc8b6f88fbf66ca065d7193aced
Nick Otonglo <nickotonglo@gmail.com>
2024-08-18 15:39:36 +0100

    added 'SystemLogs' model and migration

---
+++ database/migrations/2024_08_17_130221_create_system_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs_system', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->nullable();
+            $table->string('value')->nullable();
+            $table->text('comment')->nullable();
+            $table->integer('parent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('system_logs');
+    }
+};
